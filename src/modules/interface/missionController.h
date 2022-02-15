74e23ce9ebdbe1d928674bcbe4880c7af6ec55b7
Roesansus <Roesansus@gitlab.com>
2022-02-14 20:12:09 -0500

    Added a missionController for the takeoff and landing behaviours

---
+++ src/modules/interface/missionController.h
@@ -0,0 +1,41 @@
+/**
+ * ,---------,       ____  _ __
+ * |  ,-^-,  |      / __ )(_) /_______________ _____  ___
+ * | (  O  ) |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
+ * | / ,--´  |    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
+ *    +------`   /_____/_/\__/\___/_/   \__,_/ /___/\___/
+ *
+ * Crazyflie control firmware
+ *
+ * Copyright (C) 2019 Bitcraze AB
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, in version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * push.c - App layer application of the onboard push demo. The crazyflie 
+ * has to have the multiranger and the flowdeck version 2.
+ */
+#pragma once
+
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef enum {
+    idle,
+    lowUnlock,
+    unlocked,
+    stopping
+} State;
+
+#define DEBUG_MODULE "PUSH"
